a9d82a05543bf478fd5263ae926d4b463c536183
MostafaR <mostafamahgoub@outlook.com>
2023-07-18 18:59:02 +0300

    Finish the dark light theme for the practice screen

--- client-side/src/index.tsx
+++ client-side/src/index.tsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useRef , useState ,useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import PracticeScreen from './practice-screen/practiceScreen';
 import './index.css';
+
+function App() {
+  const [isDarkTheme, setIsDarkTheme] = useState(true);
+
+  const handleThemeToggle = () => {
+    setIsDarkTheme(!isDarkTheme);
+  };
+
+  const bodyClassName = isDarkTheme ? "background-dark" : "background-light";
+
+  useEffect(() => {
+    const body = document.querySelector("body");
+    if (body) {
+      body.className = bodyClassName;
+    }
+  }, [bodyClassName]);
+
+  return (
+    <div>
+      <PracticeScreen isDarkTheme={isDarkTheme} handleThemeToggle={handleThemeToggle} />
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <PracticeScreen />
+    <App />
   </React.StrictMode>
 );
-
--- client-side/src/practice-screen/practiceScreen.tsx
+++ client-side/src/practice-screen/practiceScreen.tsx
@@ -1,18 +1,34 @@
-
+import { useState } from "react";
 import "./practiceScreen.sass";
+import darkThemeIcon from "./assets/dark-theme.png";
+import lightThemeIcon from "./assets/light-theme.png";
 
-function practiceScreen() {
+interface PracticeScreenProps {
+    isDarkTheme: boolean;
+    handleThemeToggle: () => void;
+}
+
+function PracticeScreen({ isDarkTheme , handleThemeToggle }: PracticeScreenProps) {
+  
+
+  const themeIcon = isDarkTheme ? darkThemeIcon : lightThemeIcon;
+
+  
+
   return (
     <div className="PracticeScreen-Container">
-        <div className="button" id="Practice-Button">
+      <div className={`${isDarkTheme ? "button-dark" : "button-light"}`} id="Practice-Button">
         <div id="circle"></div>
-        <a href="#">Start your practice</a>
-        </div>
-        <div className="theme-container shadow-dark">
-
-        </div>
+        <a className={`${isDarkTheme ? "a-dark" : "a-light"}`} href="#">Start your practice</a>
+      </div>
+      <div
+        className={`theme-container themeClass`}
+        onClick={handleThemeToggle}
+      >
+        <img src={themeIcon} alt="Theme" />
+      </div>
     </div>
   );
 }
 
-export default practiceScreen;
+export default PracticeScreen;
